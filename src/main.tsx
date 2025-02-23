1c9c7ba670513813918eae98266c7d07f9b4a885
Cesar Caris <cesarantonioastete98@gmail.com>
2025-02-23 16:18:27 -0300

    Solucion al problemas de las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
